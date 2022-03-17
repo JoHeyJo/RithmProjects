865b9eec6b94e8170d48e9c39382543d49af8d66
jf <jpf0628@gmail.com>
2022-03-16 21:36:20 -0700

    added delete button &  jQuery

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 
-function getInputVal(){
+
+$("#submit-form").on("submit", (evt) => {
+  evt.preventDefault();
+  deleteImage();
+});
+
+
+$("#submit-button").on("click", getInputVal);
+
+function deleteImage() {
+  $("img").on("click", () => {
+    $("img").remove();
+  });
+}
+
+function getInputVal() {
   let inputVal = document.getElementById("userInput").value;
 
-  let imgElement= document.createElement("img");
+  let imgElement = document.createElement("img");
   imgElement.src = inputVal;
 
   let imgContainer = document.getElementById("imageHTML");
@@ -11,11 +26,8 @@ function getInputVal(){
   let topCaption = document.getElementById("text-top").value;
   document.getElementById("top-div").innerText = topCaption;
 
-
   let bottomCaption = document.getElementById("text-bottom").value;
-  document.getElementById("bottom-div").innerText = bottomCaption;  
-}
+  document.getElementById("bottom-div").innerText = bottomCaption;
 
-function reloadPage(){
-  document.location.reload();
+
 }
